46798f97b1358ef3cd131d119e7bd2cfdb7acd7c
Diorgenes Morais <diorgenesmorais@gmail.com>
2019-12-22 05:51:21 -0300

    outra forma basic security

--- src/main/java/com/example/codeblog/config/BasicSecurityConfig.java
+++ src/main/java/com/example/codeblog/config/BasicSecurityConfig.java
@@ -1,18 +1,21 @@
 package com.example.codeblog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("angular").password("{noop}@ngul@r").roles("ROLE");
+		auth.userDetailsService(userDetailsService());
 	}
 
 	@Override
@@ -23,5 +26,12 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 			.httpBasic().and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.csrf().disable();
+	}
+
+	public UserDetailsService userDetailsService() {
+	    User.UserBuilder builder = User.withDefaultPasswordEncoder();
+	    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+	    manager.createUser(builder.username("angular").password("@ngul@r").roles("ROLE").build());
+	    return manager;
 	}
 }
